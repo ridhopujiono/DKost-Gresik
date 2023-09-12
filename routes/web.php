e8e3419be5a34660162113f7ceecd289b20d5873
Ridho Pujiono <ridho@smktjp.sch.id>
2023-09-12 12:07:36 +0700

    redirect sementara

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('locations');
 });
 Route::resource('locations', LocationController::class);
 Route::resource('rooms', RoomController::class);
