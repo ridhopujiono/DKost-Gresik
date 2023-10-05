ef4c74706762daad247fbfdbe2767e10db8b43dd
Ridho Pujiono <ridho@smktjp.sch.id>
2023-10-05 12:04:47 +0700

    Ubah title

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -27,7 +27,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>D'Kost Gresik | {{ $title }}</title>
+    <title>Rumah Sidqia | {{ $title }}</title>
 
     <meta name="description" content="" />
 
